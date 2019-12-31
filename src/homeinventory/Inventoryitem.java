44b29d291f345b5fbcb683d4f8b7d170b38e3f39
Acer <Acer@DESKTOP-MNHDSO9>
2019-12-31 15:10:43 +0530

    HomeInventory project

---
+++ src/homeinventory/Inventoryitem.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeinventory;
+public class Inventoryitem
+{
+public String description;
+public String location;
+public boolean marked;
+public String serialNumber;
+public String purchasePrice;
+public String purchaseDate;
+public String purchaseLocation;
+public String note;
+public String photoFile;
+}
